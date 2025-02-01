100f7238d8e1444016cbaa2739d0eded6069465a
Pedro Palmarella <pedro_palmarella@outlook.com>
2025-02-01 14:04:20 -0300

    bootsel pelo joystick

--- semaforo_temporizado.c
+++ semaforo_temporizado.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
+#include "pico/bootrom.h"
 
 #define LED_RED 13
 #define LED_GREEN 11
+#define JOY_STICK 22
 
+volatile uint32_t last_time = 0;
 struct repeating_timer timer;
 
 bool repeating_timer_callback(struct repeating_timer *t){
@@ -24,6 +27,21 @@ bool repeating_timer_callback(struct repeating_timer *t){
 
     return true;    
 }
+// Função de interrupção com debouncing
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    // Obtém o tempo atual em microssegundos
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    // Verifica se passou tempo suficiente desde o último evento
+    if (current_time - last_time > 200000) // 200 ms de debouncing
+    {
+        if (gpio == JOY_STICK)
+        {
+            reset_usb_boot(0, 0);
+        }
+        last_time = current_time; // Atualiza o tempo do último evento        
+    }
+}
 
 int main()
 {
@@ -34,8 +52,12 @@ int main()
     gpio_set_dir(LED_RED, GPIO_OUT);
     gpio_init(LED_GREEN);
     gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_init(JOY_STICK);
+    gpio_set_dir(JOY_STICK, GPIO_IN);
+    gpio_pull_up(JOY_STICK);
     
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    gpio_set_irq_enabled_with_callback(JOY_STICK, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     while (true) {
 
         sleep_ms(1000);
